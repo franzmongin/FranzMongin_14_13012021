5c0dfb5bec9fb7a4e44f39042fee38e03e2500e8
franzmongin <33622433+franzmongin@users.noreply.github.com>
2021-12-20 16:26:51 +0100

    wip converting datatables

--- hrnet/src/components/DataTable.jsx
+++ hrnet/src/components/DataTable.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DataTable.css";
-import icon from "./sort_both.png";
 
 function DataTable({ data, columns }) {
-  console.log(data);
+  console.log("data", data);
   console.log(columns);
+  const [tabledata, settabledata] = useState(data);
+  const [activeSorting, setactiveSorting] = useState("");
   return (
-    <div className="data-table">
-      <div className="columns-titles">
-        {columns.map((column) => {
-          return (
-            <>
-              <div className="column-title">{column.title}</div>
-              <img src={icon} alt="" />
-            </>
-          );
-        })}
-      </div>
+    <div className="data-table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column) => {
+              return (
+                <React.Fragment key={`column-title${column.title}`}>
+                  <th className={`sorting sorting-${column.data}`}>
+                    {column.title}
+                  </th>
+                </React.Fragment>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((el) => {
+            console.log(el.firstName);
+            return (
+              <tr key={`${el.firstName} ${el.lastName}`}>
+                {columns.map((col) => {
+                  return (
+                    <td key={`${col.title}-${col.data}`}>{el[col.data]}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
